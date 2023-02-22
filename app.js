6336b303e5037d3e2247be6e65e8e02cac909a08
Jacob Weldon <jacob.a.weldon@gmail.com>
2023-02-22 10:39:49 -0500

    Implemented the MVC framework for our project for better organization and coding ease

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Course = require("./models/course");
-
+const courseRoutes = require("./routes/courseRoutes");
 // express app
 const app = express();
 
@@ -17,7 +16,7 @@ mongoose
   .then((result) => {
     console.log("connected to MongoDB");
     // listen for requests
-    app.listen(3000);
+    app.listen(8080);
   })
   .catch((error) => {
     console.log(error);
@@ -38,71 +37,15 @@ app.get("/", (req, res) => {
   res.redirect("/courses");
 });
 
+// TEach routes
 app.get("/teachers", (req, res) => {
   res.render("teachers", { title: "Teachers" });
 });
 
 // course routes
-app.get("/courses", (req, res) => {
-  Course.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Courses", courses: result });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+app.use("/courses", courseRoutes);
 
-app.post("/courses", (req, res) => {
-  console.log(req.body);
-  const course = new Course(req.body);
-  course
-    .save()
-    .then((result) => {
-      res.redirect("/courses");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-app.get("/courses/create", (req, res) => {
-  res.render("create", { title: "create" });
-});
-
-// handle individual course entries
-// I have having trouble with this one. When the code runs, it spits out, "Cast to ObjectId failed for value "sdfg" (type string) at path "_id" for model "Course""
-app.get("/courses/:id", (req, res) => {
-  const id = req.params.id;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).render("404", { title: "404" });
-  }
-  Course.findById(id)
-    .then((result) => {
-      if (!result) {
-        return res.status(404).render("404", { title: "404" });
-      }
-      res.render("details", { course: result, title: "Course Details" });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).send("Internal server error");
-    });
-});
-//I am also having trouble with this one. It is throwing the error "TypeError: Cannot read properties of undefined (reading 'redirect')at 63f21e937cc4528a7c796f13:95:41" but it still deletes it from the Database.
-app.delete("/courses/:id", (req, res) => {
-  const id = req.params.id;
-  Course.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/" });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
 // 404 page
-//This won't display because of the above errors
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
---
+++ routes/courseRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const courseController = require("../controllers/courseController");
+
+router.get("/", courseController.course_index);
+
+router.post("/", courseController.course_create_post);
+
+router.get("/create", courseController.course_create_get);
+
+router.get("/:id/update", courseController.course_update_get);
+
+// handle individual course entries
+router.get("/:id", courseController.course_details);
+
+router.delete("/:id", courseController.course_delete);
+
+module.exports = router;
